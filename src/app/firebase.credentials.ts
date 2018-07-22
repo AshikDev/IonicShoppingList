145b7626beb6d107c3fe4ac8f2d6dd33ec609456
Md Ashikur Rahman <ashik.soft.eng@gmail.com>
2018-07-22 12:40:08 +0600

    v1

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDDzew92dUsEV34CVHzgjHNzNYuoF-wl9g",
+    authDomain: "ionicshoppinglist-fd48a.firebaseapp.com",
+    databaseURL: "https://ionicshoppinglist-fd48a.firebaseio.com",
+    projectId: "ionicshoppinglist-fd48a",
+    storageBucket: "ionicshoppinglist-fd48a.appspot.com",
+    messagingSenderId: "141316580913"
+  };
